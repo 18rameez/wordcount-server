6b136a82dc1b8e25e3d8c00f5a2b4d2f47a25eaf
18rameez <rammipatla@gmail.com>
2023-03-23 17:56:17 +0530

    route change

--- src/routes/word-count.js
+++ src/routes/word-count.js
@@ -6,7 +6,7 @@ const verifyToken = require("../middleware/jwtverify")
 router.post('/', wordCountController.getWordCount)
 //getting history of a user by id
 router.get('/history/:id', verifyToken, wordCountController.getHistory)
-router.patch('/favourite/:id',  wordCountController.addToFavorites)
+router.put('/favourite/:id',  wordCountController.addToFavorites)
 router.delete('/history/:id', wordCountController.deleteHistoryById);
     
 
